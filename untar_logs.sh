a6e149b86fe36818d4081b336a09e8083781dddf
Steven Green <sgreen@dunegpvm01.fnal.gov>
2018-06-28 09:37:33 -0500

    Untar only the relevant file(s).

--- untar_logs.sh
+++ untar_logs.sh
@@ -3,6 +3,6 @@
 for dir in */
 do 
     cd $dir
-    [[ -f Pandora_Events.pndr ]] && echo "Found" || tar -xvf log.tar
+    [[ -f Pandora_Events.pndr ]] && echo "Found" || tar -xvf log.tar ./Pandora_Events.pndr
     cd ../
 done
